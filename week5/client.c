ec36737e2d1c19b050c9ce7334daa2a4e10a63ec
Evsey <evseyru1@mail.ru>
2023-02-22 16:02:28 +0300

    added week5 lab

---
+++ week5/client.c
@@ -0,0 +1,56 @@
+// Client side implementation of UDP client-server model
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+#define PORT	 2000
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	const message[MAXLINE];
+	struct sockaddr_in	 servaddr;
+
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+
+	// Filling server information
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+	int n;
+	socklen_t len;
+
+	while (1) {
+		printf("Input number: ");
+		int num = 0;
+		scanf("%d", &num);
+		sprintf((char *) message, "%d", num);
+		sendto(sockfd, (const char *)message, strlen(message),
+			   MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+			   sizeof(servaddr));
+		printf("Message sent.\n");
+
+		n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+					 MSG_WAITALL, (struct sockaddr *) &servaddr,
+					 &len);
+		buffer[n] = '\0';
+		printf("Server : %s\n", buffer);
+		if (strcmp(buffer, "WIN") == 0 || strcmp(buffer, "LOSE") == 0) {
+			break;
+		}
+
+	}
+	close(sockfd);
+	return 0;
+}
